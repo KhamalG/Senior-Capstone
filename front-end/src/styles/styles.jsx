697ef5017dd36621b5333a3bc142475b05b52e14
Merith <hutyeraba@me.com>
2024-03-22 15:52:20 -0400

    friday march 22 login page

---
+++ front-end/src/styles/styles.jsx
@@ -0,0 +1,21 @@
+const styles = {
+    loginBody: {
+        color: 'black',
+        fontFamily: 'Courier',
+        justifyContent:'center'
+    },
+    header: {
+        color: 'black',
+        fontFamily: 'Courier',
+        backgroundColor: 'white'
+
+    },
+    label: {
+        fontFamily: 'Courier',
+    },
+    button: {
+        fontFamily: 'Courier'
+    }
+}
+
+export default styles;
