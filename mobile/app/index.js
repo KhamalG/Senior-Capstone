9eeee370baf477e6ccca7980ff048560a7b896ba
Khamal Goodwin <kgoodwi6@rockets.utoledo.edu>
2024-03-28 13:47:28 -0400

    correct mobile setup

---
+++ mobile/app/index.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>
+                LoginScreen
+            </Text>
+        </View>
+    );
+};
+
+export default LoginScreen;
