c3e55dfed80726792309f0063050c31026762b05
Khamal Goodwin <kgoodwi6@rockets.utoledo.edu>
2024-03-28 13:55:59 -0400

    server stuff

--- server/credentials.py
+++ server/credentials.py
@@ -1,2 +1,4 @@
 username = 'drive5'
 password = 'Senior24'
+server = 'team-a5.database.windows.net'
+database = 'team-a5'
--- server/main.py
+++ server/main.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
+import pypyodbc as odbc
+from credentials import username, password, server, database
 
+connection_string = '{ODBC Driver 18 for SQL Server};Server=tcp:team-a5.database.windows.net,1433;Database=team-a5;Uid=team-a5;Pwd='+password+';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+conn = odbc.connect(connection_string)
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/admin/login", methods=["POST"])
+def login():
+    if request.method == "POST":
+        return "new"
     return "Home"
 
 if __name__ == "__main__":
